28a224fa3aba211d66039991dcc040845266ea81
Gwendolyn Clark <46104572+spingle@users.noreply.github.com>
2019-10-11 15:09:37 -0500

    Implemented matrix multiplication

--- scripts/matrix.js
+++ scripts/matrix.js
@@ -34,9 +34,19 @@ class Matrix {
         var result = null;
         // ensure multiplication is valid
         if (rhs instanceof Matrix && this.columns === rhs.rows) {
-            // implement matrix multiplication here!
+            var resultArray = [] 
+            for (var n = 0;n<this.rows;n++) //define a nulled 2d array to be used as our valueset for result 
+                resultArray.push(new Array(rhs.columns).fill(null)); //needs to be null because of some stupidity later
+
+            for (var i =0; i < this.rows;i++){
+                dotProductRow(this.data[i],rhs,resultArray); //loop through and feed each row in our matrix to our per-row dot product function
+            }
+            result = new Matrix(this.rows,rhs.columns); 
+            result.values = resultArray;//once our array is finished, set our result's values to it. done!!
+            console.log(result);
         }
         else {
+
             console.log("could not multiply - row/column mismatch");
         }
         return result;
@@ -59,4 +69,27 @@ Matrix.multiply = function(...args) {
         console.log("could not multiply - requires at least 2 matrices");
     }
     return result;
+}
+
+function dotProductRow(row, matrix, result){
+    var sum = 0;
+    var nullfound; //prevents overwriting of result array
+    for (var i =0; i < matrix.columns;i++){ // O(yikes)
+        nullfound = false; 
+        for (var j=0; j< row.length;j++){ //sums each row entry w each entry of given column
+          console.log("M1 VAL: "+ row[j] +" M2 VAL: "+matrix.data[j][i]);   
+          sum+=row[j]*matrix.data[j][i]; 
+        }
+        //loop through our result array, insert @ first null, then ensure we don't do it again until next row
+        //this is incredibly stupid 
+        for (var k=0;k<result.length;k++){ 
+            for (var l=0; l<result[k].length;l++){
+                if (result[k][l]===null & !nullfound){
+                    result[k][l]=sum;
+                    nullfound = true;
+                }
+            }
+        }
+        sum=0;
+    }
 }
